5c0099eb09f4cb7563263d06c9848bcd12c6a8b1
spoot <spoot@bk.ru>
2025-09-21 21:32:20 +0300

    03-hw

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script setup>
+import Header from './components/Header.vue';
+import Score from './components/Score.vue';
 import Button from './components/Button.vue';
+import Card from './components/Card.vue';
+
+
+const onFlip = () => {
+    console.log('Карта перевернулась')
+}
+
+const onStatusChange = () => {
+    console.log('Статус изменен')
+}
+
 </script>
 
 <template>
+    <Header>
+        <h1>ЗАПОМНИ СЛОВО</h1>
+        <Score cnt="100"></Score>
+    </Header>
+
     <main>
         <Button>Начать игру</Button>
+
+        <Card @flip="onFlip" @statusChange="onStatusChange"></Card>
     </main>
+
 </template>
 
 <style scoped></style>
